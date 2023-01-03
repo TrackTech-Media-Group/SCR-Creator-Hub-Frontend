523571d157f179bb1a91da7e8a3aab51b9cb0fce
Daan Klarenbeek <github@ijskoud.dev>
2023-01-03 17:34:36 +0100

    feat: add mobile support (#10)
    * feat(Home): make home page accessible for mobile
    
    * feat(Navbar): add mobile design
    
    * feat(Buttons): allow onClick event for links
    
    * feat(Navbar): add mobile navbar

--- packages/navbar/src/HomeNavbar.tsx
+++ packages/navbar/src/HomeNavbar.tsx
@@ -1,16 +1,26 @@
 import { TransparentButton, WhiteButton } from "@creatorhub/buttons";
-import type React from "react";
+import React, { useState } from "react";
+import { HomeMobileMenu } from "./HomeMobileMenu";
+import MenuButton from "./MenuButton";
 
 export const HomeNavbar: React.FC = () => {
+	const [mobileMenu, setMobileMenu] = useState(false);
+	const toggleMenu = () => setMobileMenu(!mobileMenu);
+	const closeMenu = () => setMobileMenu(false);
+
 	return (
 		<div className="fixed z-[100] w-screen h-24 py-4 px-8 flex justify-between items-center">
-			<img src="/logo/logo.png" className="h-full" />
-			<div className="flex gap-11">
+			<img src="/logo/logo.png" alt="Creator Hub Logo" className="h-full" />
+			<div className="flex gap-11 max-md:hidden">
 				<TransparentButton type="button">Products</TransparentButton>
 				<TransparentButton type="button">Resources</TransparentButton>
 				<WhiteButton type="link" href="/login" extra="rounded-full h-fit">
 					Sign in <i className="fa-solid fa-arrow-right-long" />
 				</WhiteButton>
+			</div>
+			<div className="hidden max-md:block">
+				<HomeMobileMenu active={mobileMenu} closeMenu={closeMenu} />
+				<MenuButton onClick={toggleMenu} active={mobileMenu} />
 			</div>
 		</div>
 	);
