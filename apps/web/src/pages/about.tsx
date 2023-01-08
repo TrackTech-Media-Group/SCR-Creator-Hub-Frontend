d9482004e436a36213187f859dd7c233b7ad7d19
ijsKoud <github@ijskoud.dev>
2023-01-08 19:48:45 +0100

    feat(About): add projects and partners list

--- apps/web/src/pages/about.tsx
+++ apps/web/src/pages/about.tsx
@@ -4,8 +4,8 @@ export default function About() {
 	return (
 		<>
 			<HomeNavbar />
-			<div className="px-32 max-md:px-4 min-h-screen flex flex-col justify-center items-center">
-				<div className="max-w-[750px] px-4">
+			<div className="px-32 pt-40 max-md:px-4 min-h-screen flex flex-col justify-center items-center">
+				<div className="max-w-[750px] px-4 flex flex-col gap-8">
 					<div className="flex flex-col gap-2">
 						<h1 className="text-title">About Us</h1>
 						<p className="text-base">
@@ -16,6 +16,28 @@ export default function About() {
 							We provide services in many sectors of content creation, from graphic design to videos. The majority of our work is Roblox
 							related.
 						</p>
+					</div>
+					<div className="flex flex-col gap-4">
+						<h1 className="text-xl">Projects</h1>
+						<div>
+							<img src="/projects/trackmesh.png" alt="TrackMesh logo" />
+							<p className="text-base">
+								The future of models on Roblox. Using advanced 3D scanning photogrammetry, in combination with the TrackMesh API,
+								creates unreal models that push the Roblox engine to it&apos;s limits.
+							</p>
+						</div>
+						<div>
+							<img src="/projects/captions.png" alt="Captions by TrackTech logo" />
+							<p className="text-base">
+								Captions By TrackTech (previously TrackCC) is a smart captioning service for content creators.
+							</p>
+						</div>
+					</div>
+					<div className="flex flex-col gap-4">
+						<h1 className="text-xl">Partners</h1>
+						<div>
+							<img src="/partners/bantech.png" alt="Bantech logo" />
+						</div>
 					</div>
 				</div>
 			</div>
