de520cb701a5a75f08283b6bfe4f1d98f1f0277b
ijsKoud <github@ijskoud.dev>
2023-01-15 19:21:08 +0100

    feat(Images): add cards

--- apps/web/src/pages/images/index.tsx
+++ apps/web/src/pages/images/index.tsx
@@ -1,5 +1,21 @@
+import { HomeCard } from "@creatorhub/cards";
 import { MediaLayout } from "@creatorhub/ui";
 
 export default function ImagesHome() {
-	return <MediaLayout></MediaLayout>;
+	return (
+		<MediaLayout>
+			<h1 className="text-3xl text-center">See what&apos;s trending</h1>
+			<div className="flex flex-wrap gap-4 justify-center">
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+				<HomeCard type="video" alt="cards_placeholder_image" src="/cards_placeholder_image.png" href="/images/cards_placeholder_image.png" />
+			</div>
+		</MediaLayout>
+	);
 }
