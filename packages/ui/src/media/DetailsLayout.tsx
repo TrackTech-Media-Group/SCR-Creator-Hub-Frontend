204e9a56054b38a382ad1cc68453fd46dec648d9
ijsKoud <github@ijskoud.dev>
2023-01-19 21:53:21 +0100

    feat(Ui): add media details layout

---
+++ packages/ui/src/media/DetailsLayout.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+import { HomeNavbar } from "@creatorhub/navbar";
+
+export const MediaDetailsLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+	return (
+		<>
+			<HomeNavbar />
+			<div className="px-32 pt-24 max-md:px-16 max-sm:px-4">{children}</div>
+		</>
+	);
+};
