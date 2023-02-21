d85e73be97920815981243e5ce04d4367595a8cf
ijsKoud <github@ijskoud.dev>
2023-02-21 21:55:48 +0100

    feat: add @creatorhub/swr

---
+++ packages/swr/src/useSwr.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import UseSwr from "swr";
+import type { FetcherResponse, PublicConfiguration } from "swr/_internal";
+
+export const defaultFetcher = (url: string) =>
+	axios.get(`${process.env.NEXT_PUBLIC_API_URL}${url}`, { withCredentials: true }).then((res) => res.data);
+
+export const useSwr = <Response = any, Error = any>(
+	url: string,
+	options?: Partial<PublicConfiguration<Response, Error, (args: string) => FetcherResponse<Response>>>,
+	fetcher = defaultFetcher
+) => {
+	const swr = UseSwr<Response, Error, string>(url, fetcher, { revalidateOnFocus: false, ...options });
+	return swr;
+};
+
+export const useSwrWithUpdates = <Response = any, Error = any>(
+	url: string,
+	options?: Partial<PublicConfiguration<Response, Error, (args: string) => FetcherResponse<Response>>>,
+	fetcher = defaultFetcher
+) => {
+	const swr = UseSwr<Response, Error, string>(url, fetcher, { refreshInterval: 5e3, revalidateOnFocus: false, ...options });
+	return swr;
+};
