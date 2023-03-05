0db3932819dd1d93818325de4ea5f9884fd459a1
ijsKoud <github@ijskoud.dev>
2023-03-05 11:22:34 +0100

    feat(SideMenu): add transitions

--- packages/navbar/src/admin/SideMenu.tsx
+++ packages/navbar/src/admin/SideMenu.tsx
@@ -1,15 +1,66 @@
 import { SecondaryButton, TransparentButton } from "@creatorhub/buttons";
 import type React from "react";
+import { motion, useAnimation, Variants } from "framer-motion";
+import { useEffect } from "react";
 
 interface Props {
 	closeMenu: () => void;
 	active: boolean;
 }
 
+const TopVariants: Variants = {
+	initial: {
+		opacity: 0,
+		transition: { duration: 0.25, ease: [0.4, 0, 0.2, 1] }
+	},
+	animate: {
+		opacity: 1,
+		transition: { duration: 0.25, ease: [0.4, 0, 0.2, 1] }
+	},
+	exit: {
+		opacity: 0,
+		transition: { duration: 0.25, ease: [0.4, 0, 0.2, 1] }
+	}
+};
+
+const SideVariants: Variants = {
+	initial: {
+		transform: "translateX(-320px)",
+		transition: { duration: 0.25, ease: [0.4, 0, 0.2, 1] }
+	},
+	animate: {
+		transform: "translateX(0px)",
+		transition: { duration: 0.25, ease: [0.4, 0, 0.2, 1] }
+	},
+	exit: {
+		transform: "translateX(-320px)",
+		transition: { duration: 0.25, ease: [0.4, 0, 0.2, 1] }
+	}
+};
+
 export const SideMenu: React.FC<Props> = ({ active, closeMenu }) => {
+	const animateController = useAnimation();
+
+	useEffect(() => {
+		if (active) void animateController.start("animate");
+		else void animateController.start("exit");
+
+		return () => animateController.stop();
+	}, [active]);
+
 	return (
-		<div className={`w-screen h-screen z-10 left-0 top-0 ${active ? "fixed" : "hidden"} backdrop-blur`}>
-			<div className="absolute w-80 h-screen bg-main pt-20 flex flex-col px-4 gap-4">
+		<motion.div
+			variants={TopVariants}
+			initial="initial"
+			animate={animateController}
+			className={`w-screen h-screen z-10 left-0 top-0 fixed ${active ? "pointer-events-auto" : "pointer-events-none"} backdrop-blur`}
+		>
+			<motion.div
+				variants={SideVariants}
+				initial="initial"
+				animate={animateController}
+				className="absolute w-80 h-screen bg-main pt-20 flex flex-col px-4 gap-4"
+			>
 				<div>
 					<h1 className="text-xl font-bold">Products</h1>
 					<div className="flex flex-col gap-2 pl-2">
@@ -55,7 +106,7 @@ export const SideMenu: React.FC<Props> = ({ active, closeMenu }) => {
 						Sign in <i className="fa-solid fa-arrow-right-long" />
 					</p>
 				</SecondaryButton>
-			</div>
-		</div>
+			</motion.div>
+		</motion.div>
 	);
 };
