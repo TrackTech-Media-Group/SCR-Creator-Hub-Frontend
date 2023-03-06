ef86ea1c5c37ff85b809140786440c48d42dca8f
ijsKoud <github@ijskoud.dev>
2023-03-06 21:25:36 +0100

    feat: add videos pages

---
+++ apps/web/src/pages/videos/index.tsx
@@ -0,0 +1,24 @@
+import { useSwrWithUpdates } from "@creatorhub/swr";
+import { DisplaySection, MediaLayout } from "@creatorhub/ui";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+const VideosHome: NextPage = () => {
+	const [tags, setTags] = useState<{ id: string; name: string }[]>([]);
+	const { data: tagData } = useSwrWithUpdates<{ id: string; name: string }[]>("/admin/tags");
+	useEffect(() => {
+		if (tagData) setTags(tagData);
+	}, [tagData]);
+
+	return (
+		<MediaLayout>
+			<div className="flex flex-col items-center justify-center gap-y-32 pb-8">
+				{tags.map((tag) => (
+					<DisplaySection key={tag.id} tag={tag.name} id={tag.id} type="video" />
+				))}
+			</div>
+		</MediaLayout>
+	);
+};
+
+export default VideosHome;
