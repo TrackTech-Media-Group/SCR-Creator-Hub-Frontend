e50c609a32b6c3da437f641bea33be07fb5c6e2e
ijsKoud <github@ijskoud.dev>
2023-03-26 17:35:29 +0200

    fix: incorrect login redirect

--- apps/web/src/pages/images/[id].tsx
+++ apps/web/src/pages/images/[id].tsx
@@ -32,7 +32,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 	if (!userSession)
 		return {
-			redirect: "/login",
+			redirect: { destination: "/login", permanent: false },
 			props: {}
 		};
 	console.log("test");
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	});
 	if (!csrf.data.token.length)
 		return {
-			redirect: "/login",
+			redirect: { destination: "/login", permanent: false },
 			props: {}
 		};
 
