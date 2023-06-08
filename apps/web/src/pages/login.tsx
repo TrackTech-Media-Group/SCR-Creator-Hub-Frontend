259dd64a6e9856b7f8c715b191c217296848b667
Daan Klarenbeek <github@ijskoud.dev>
2023-06-08 14:14:06 +0200

    feat: add login page

---
+++ apps/web/src/pages/login.tsx
@@ -0,0 +1,20 @@
+import type { GetServerSideProps } from "next";
+import { getOauth2, setCookie } from "@creatorhub/utils";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const data = await getOauth2();
+	setCookie("XSRF-STATE-TOKEN", data.token, {
+		req: ctx.req,
+		res: ctx.res
+	});
+
+	return {
+		redirect: {
+			permanent: true,
+			destination: data.url
+		}
+	};
+};
+
+const Login = () => <div></div>;
+export default Login;
