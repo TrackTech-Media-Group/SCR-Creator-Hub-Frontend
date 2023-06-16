377a5a0cc83fa46402d831fe0c54a88718905c1b
Daan Klarenbeek <github@ijskoud.dev>
2023-06-16 10:02:24 +0200

    chore: add aditional sitemap

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
 	siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://scrcreate.app",
+	exclude: ["/admin", "/admin/*", "/api/*", "/login", "/logout", "/500", "/400"],
 	generateRobotsTxt: true,
-	exclude: ["/admin", "/admin/*", "/api/*", "/login", "/logout"]
+	robotsTxtOptions: {
+		additionalSitemaps: ["https://api.scrcreate.app/sitemap.xml"]
+	}
 };
 
 export default config;
