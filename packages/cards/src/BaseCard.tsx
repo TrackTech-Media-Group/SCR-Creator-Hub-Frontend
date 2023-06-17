7c1ed0a156d8e871f9faa2f663b1f2f36d5de1c0
Daan Klarenbeek <github@ijskoud.dev>
2023-06-17 14:50:25 +0200

    feat(Cards): add basecard component

---
+++ packages/cards/src/BaseCard.tsx
@@ -0,0 +1,24 @@
+import type { Type } from "@creatorhub/utils";
+import React from "react";
+import { ProgressiveImage } from "./Image";
+
+export interface CardProps {
+	/** The content type */
+	type: Type;
+
+	/** The preview url */
+	src: string;
+
+	/** The name of the content item */
+	name: string;
+}
+
+export const BaseCard: React.FC<React.PropsWithChildren<CardProps>> = ({ src, name, children }) => {
+	return (
+		<div className="w-80 rounded-lg overflow-hidden relative cursor-pointer outline outline-transparent hover:outline-white transition-all z-10">
+			<ProgressiveImage loading="lazy" width={320} height={180} src={src} alt={name} />
+			<p className="absolute bottom-0 left-0 w-full text-base bg-gradient-to-t from-black-900 to-transparent p-2">{name}</p>
+			{children}
+		</div>
+	);
+};
