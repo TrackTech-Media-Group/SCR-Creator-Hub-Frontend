b0b389e37fc4366bf47bc01b6fee465683fc79a5
Daan Klarenbeek <github@ijskoud.dev>
2023-06-17 20:05:39 +0200

    feat: update next-sitemap generation

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -2,6 +2,13 @@
 const config = {
 	siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://scrcreate.app",
 	exclude: ["/admin", "/admin/*", "/api/*", "/login", "/logout", "/500", "/400"],
+	transform: (config, url) => {
+		const high = ["/", "/image", "/video", "/music"].some((str) => url.endsWith(str)); // 1.0;
+		const medium = ["/about", "/search"].some((str) => url.endsWith(str)); // 0.7;
+		const low = ["/support/faq", "/profile"].some((str) => url.endsWith(str)); // 0.3;
+
+		return { loc: url, changefreq: "daily", priority: high ? 1.0 : medium ? 0.7 : low ? 0.3 : 0.2 };
+	},
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
 		additionalSitemaps: ["https://api.scrcreate.app/sitemap.xml"]
